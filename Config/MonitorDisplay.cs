5107ca71b2ee115013768d5d5d12320bc64c76cf
Cara <git@caraxian.com>
2020-09-18 15:40:37 +0930

    Remove unused using statements

--- Config/MonitorDisplay.cs
+++ Config/MonitorDisplay.cs
@@ -1,11 +1,7 @@
 ﻿using ImGuiNET;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-using Lumina.Excel.GeneratedSheets;
 using Newtonsoft.Json;
 
 namespace RemindMe.Config {
