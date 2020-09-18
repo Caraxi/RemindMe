cdda3aa66360a3949ced43b04c2e9a79458d8a86
Cara <git@caraxian.com>
2020-09-18 11:27:16 +0930

    Fix hack/workaround

--- ActorExtension.cs
+++ ActorExtension.cs
@@ -5,7 +5,7 @@ using Dalamud.Game.ClientState.Structs;
 
 namespace RemindMe {
     internal static class ActorExtension {
-        private static readonly FieldInfo actorStruct = typeof(Actor).GetField("actorStruct");
+        private static readonly FieldInfo actorStruct = typeof(Actor).GetField("actorStruct", BindingFlags.NonPublic | BindingFlags.Instance);
 
         public static StatusEffect[] GetStatusEffects(this Actor a) {
             return ((ActorStruct) actorStruct.GetValue(a)).UIStatusEffects;
