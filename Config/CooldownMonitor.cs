90fc757eff615b4c376d3ee5636d68620847240d
Cara <git@caraxian.com>
2020-09-23 07:25:13 +0930

    Cleanup unused references

--- Config/CooldownMonitor.cs
+++ Config/CooldownMonitor.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Action = Lumina.Excel.GeneratedSheets.Action;
-
+﻿
 namespace RemindMe.Config {
     public class CooldownMonitor {
         public uint ActionId = 0;
