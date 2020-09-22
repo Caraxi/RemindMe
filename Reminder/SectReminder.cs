735a30af477507fb8d235b4f0d47515c57077308
Cara <git@caraxian.com>
2020-09-22 11:42:45 +0930

    Sect Reminder

---
+++ Reminder/SectReminder.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Dalamud.Plugin;
+using Newtonsoft.Json;
+using RemindMe.Config;
+using Action = Lumina.Excel.GeneratedSheets.Action;
+
+namespace RemindMe.Reminder {
+    internal class SectReminder : GeneralReminder {
+
+        [JsonIgnore]
+        public override string Name => "Sect Reminder";
+
+        [JsonIgnore]
+        public override string Description => "Reminds you to apply a Sect when playing astrologian.";
+
+        public override string GetText(DalamudPluginInterface pluginInterface, RemindMe plugin, MonitorDisplay display) {
+            return "Apply Sect";
+        }
+
+        public override bool ShouldShow(DalamudPluginInterface pluginInterface, RemindMe plugin, MonitorDisplay display) {
+            return pluginInterface.ClientState.LocalPlayer.ClassJob.Id == 33 &&
+                   pluginInterface.ClientState.LocalPlayer.Level >= 30 &&
+                   pluginInterface.ClientState.LocalPlayer.GetStatusEffects().All(s => s.EffectId != 839 && s.EffectId != 840);
+        }
+
+        public override ushort GetIconID(DalamudPluginInterface pluginInterface, RemindMe plugin, MonitorDisplay display) {
+            try {
+                return pluginInterface.Data.Excel.GetSheet<Action>().GetRow(16559).Icon;
+            } catch {
+                return 0;
+            }
+        }
+
+    }
+}
