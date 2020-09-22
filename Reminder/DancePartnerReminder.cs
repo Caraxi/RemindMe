2f1621086df67661a27c92b0d53b80c8cdf8e953
Cara <git@caraxian.com>
2020-09-22 11:28:47 +0930

    Add Dance Partner Reminder

---
+++ Reminder/DancePartnerReminder.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Dalamud.Plugin;
+using Newtonsoft.Json;
+using RemindMe.Config;
+using Action = Lumina.Excel.GeneratedSheets.Action;
+
+namespace RemindMe.Reminder {
+    internal class DancePartnerReminder : GeneralReminder {
+
+        [JsonIgnore]
+        public override string Name => "Dance Partner Reminder";
+
+        [JsonIgnore]
+        public override string Description => "Reminds you to select a dance partner.";
+
+        public override string GetText(DalamudPluginInterface pluginInterface, RemindMe plugin, MonitorDisplay display) {
+            return "Dance Partner";
+        }
+
+        public override bool ShouldShow(DalamudPluginInterface pluginInterface, RemindMe plugin, MonitorDisplay display) {
+            return pluginInterface.ClientState.LocalPlayer.ClassJob.Id == 38  && 
+                   pluginInterface.ClientState.LocalPlayer.Level >= 60 && 
+                   pluginInterface.ClientState.LocalPlayer.GetStatusEffects().All(s => s.EffectId != 1823);
+        }
+
+        public override ushort GetIconID(DalamudPluginInterface pluginInterface, RemindMe plugin, MonitorDisplay display) {
+            try {
+                return pluginInterface.Data.Excel.GetSheet<Action>().GetRow(16006).Icon;
+            } catch {
+                return 0;
+            }
+        }
+
+    }
+}
