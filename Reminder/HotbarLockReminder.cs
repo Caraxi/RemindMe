e5cfcd1fc7634412ca0ea8bc15a2b4e5266412bd
Cara <git@caraxian.com>
2020-09-28 14:03:02 +0930

    Add reminder to lock hotbar

---
+++ Reminder/HotbarLockReminder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Dalamud.Plugin;
+using Newtonsoft.Json;
+using RemindMe.Config;
+
+namespace RemindMe.Reminder {
+    internal class HotbarLockReminder : GeneralReminder {
+
+        [JsonIgnore]
+        public override string Name => "Hotbar Lock Reminder";
+
+        [JsonIgnore]
+        public override string Description => "Reminds you to lock your hotbar.";
+
+        public override string GetText(DalamudPluginInterface pluginInterface, RemindMe plugin, MonitorDisplay display) {
+            return "Lock Hotbar";
+        }
+
+        public override bool ShouldShow(DalamudPluginInterface pluginInterface, RemindMe plugin, MonitorDisplay display) {
+            var actionBar = pluginInterface.Framework.Gui.GetUiObjectByName("_ActionBar", 1);
+            if (actionBar == IntPtr.Zero) return false;
+            return Marshal.ReadByte(actionBar, 0x23F) == 0;
+        }
+
+        public override ushort GetIconID(DalamudPluginInterface pluginInterface, RemindMe plugin, MonitorDisplay display) {
+            return 5; //60840;
+        }
+
+    }
+}
