81c567c1f8ef0ad392660a8f5582f7e6312bb53c
Cara <git@caraxian.com>
2020-09-30 04:00:14 +0930

    Fix pulse with longer update intervals

--- Common.cs
+++ Common.cs
@@ -9,7 +9,7 @@ namespace RemindMe {
         public Vector4 GetBarBackgroundColor(MonitorDisplay display, DisplayTimer timer) {
             if (!timer.IsComplete) return display.BarBackgroundColor;
             if (!display.PulseReady) return timer.FinishedColor;
-            var s = Math.Abs((Math.Abs(timer.TimerRemaining / (2.5f - display.PulseSpeed)) - (float)Math.Floor(Math.Abs(timer.TimerRemaining / (2.5f - display.PulseSpeed))) - 0.5f) / 2) * display.PulseIntensity;
+            var s = (float) Math.Abs((Math.Abs((timer.TimerRemaining - display.CacheAge.TotalSeconds) / (2.5f - display.PulseSpeed)) - (float)Math.Floor(Math.Abs((timer.TimerRemaining - display.CacheAge.TotalSeconds) / (2.5f - display.PulseSpeed))) - 0.5f) / 2) * display.PulseIntensity;
             if (timer.FinishedColor.W < 0.75) {
                 return timer.FinishedColor + new Vector4(0, 0, 0, s);
             }
