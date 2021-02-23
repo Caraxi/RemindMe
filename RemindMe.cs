4197da1b23fbbdcece0c45cb3a6a863bf49ad308
Cara <git@caraxian.com>
2021-02-24 05:20:30 +1030

    Just fucking build

--- RemindMe.cs
+++ RemindMe.cs
@@ -108,7 +108,7 @@ namespace RemindMe {
         private void FrameworkUpdate(Framework framework) {
             try {
                 if (PluginInterface.ClientState == null || PluginInterface.ClientState.LocalContentId == 0) return;
-                var inCombat = PluginInterface.ClientState?.LocalPlayer!.IsStatus(StatusFlags.InCombat);
+                var inCombat = PluginInterface.ClientState!.LocalPlayer!.IsStatus(StatusFlags.InCombat);
                 if (OutOfCombatTimer.IsRunning && inCombat) {
                     generalStopwatch.Restart();
                     ActionManager.ResetTimers();
